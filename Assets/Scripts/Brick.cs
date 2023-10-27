dd4ad709d2efbd49a13c62142fe740f80a836739
LiveDieRepeater <81476895+LiveDieRepeater@users.noreply.github.com>
2023-10-27 11:45:53 +0200

    Added SceneLoading & Prefabs & Basic Scenes

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Brick : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static int activeBricks = 0;
+
+    private void Awake()
     {
-        
+        activeBricks++;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        activeBricks--;
+
+        if (activeBricks == 0)
+        {
+            print("All bricks destroyed");
+            LoadNextScene();
+        }
+        DestroyObject(gameObject);
+    }
+
+    // TODO: move this somewhere else?
+    void LoadNextScene()
+    {
+        int currentSceneBuildIndex = SceneManager.GetActiveScene().buildIndex;
+        int targetSceneBuildIndex = currentSceneBuildIndex + 1;
+
+        //check if target index is out of bounds.   [When there is no more next scene, set targetSceneBuildIndex to first scene]
+        if (targetSceneBuildIndex >= SceneManager.sceneCountInBuildSettings)
+        {
+            targetSceneBuildIndex = 0;
+        }
+
+        SceneManager.LoadScene(targetSceneBuildIndex);
     }
 }
