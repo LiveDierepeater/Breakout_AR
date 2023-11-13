34b9907c62e8080c06767d4c562569cb60fd84d9
LiveDieRepeater <81476895+LiveDieRepeater@users.noreply.github.com>
2023-11-13 08:54:13 +0100

    Ball & Player now have dependencies on Rigidbody2D Component

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Ball : MonoBehaviour
 {
     public delegate void BallOutDelegate();
