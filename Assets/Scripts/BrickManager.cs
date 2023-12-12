d9bb45a055b96eaa72d307ee47cde759dff69fa9
LiveDierepeater <81476895+LiveDierepeater@users.noreply.github.com>
2023-12-12 13:02:02 +0100

    Implemented Difficulty in BrickManager.cs

--- Assets/Scripts/BrickManager.cs
+++ Assets/Scripts/BrickManager.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class BrickManager : MonoBehaviour
 {
@@ -110,11 +112,6 @@ public class BrickManager : MonoBehaviour
         int omitIndex = 13 - currentWaveNumber;
         if (omitIndex < 0) omitIndex = 0;
 
-        // Updating the currentDifficulty
-        currentDifficulty = currentWaveNumber;
-        
-        // TODO: The "currentDifficulty" will decide here how many strong bricks will get spawned.
-        
         // Create standard brick formation.
         
         for (int x = 0; x < rows; x++)
@@ -122,9 +119,7 @@ public class BrickManager : MonoBehaviour
             for (int y = 0; y < columns; y++)
             {
                 Brick newBrick = Instantiate(defaultBrickPrefab);
-
                 newBrick.OnBrickHit += Brick_OnBrickHit;
-
                 Vector3 brickScale = newBrick.transform.localScale;
                 
                 newBrick.transform.position = transform.position
@@ -149,8 +144,58 @@ public class BrickManager : MonoBehaviour
                 brick.gameObject.SetActive(false);
             }
         }
+        
+        MixStrongerBricksWithin();
     }
-    
+
+    private void MixStrongerBricksWithin()
+    {
+        // The "currentDifficulty" will decide here how many strong bricks will get spawned.
+        
+        currentDifficulty = currentWaveNumber - 2;
+        
+        if (currentDifficulty > 0)
+        {
+            // Consider "currentDifficulty" for the amount of stronger spawned bricks till a maximum difficulty (value) is reached.
+            // Goes Through every active Bricks in "brickArray" and switches them when chance is given.
+
+            for (int x = 0; x < rows; x++)
+            {
+                for (int y = 0; y < columns; y++)
+                {
+                    print(brickArray[x, y]);
+                    print("y: " + y);
+                    print("x: " + x);
+                    Brick brick = brickArray[x, y];
+                    if (brick.isActiveAndEnabled)
+                    {
+                        float switchChance = (float)currentDifficulty / 15f;
+                        float roll = Random.Range(0f, 1f);
+
+                        print("switchChance: " + switchChance);
+                        print("roll: " + roll);
+
+                        if (roll < switchChance) // Switches the current Brick out.
+                        {
+                            Brick newStrongerBrick = Instantiate(strongBrickPrefab);
+                            newStrongerBrick.OnBrickHit += Brick_OnBrickHit;
+                            Vector3 brickScale = newStrongerBrick.transform.localScale;
+                            
+                            newStrongerBrick.transform.position = transform.position
+                                                                  + (Vector3.right * y * brickScale.x)
+                                                                  + (Vector3.right * y * padding.x)
+                                                                  + (Vector3.up * x * brickScale.y)
+                                                                  + (Vector3.up * x * padding.y);
+
+                            brickArray.SetValue(newStrongerBrick, x, y);
+                            Destroy(brick.gameObject);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     private bool AreAnyBricksActive()
     {
         // Goes through "brickArray" and returns false when each brick is inactive.
